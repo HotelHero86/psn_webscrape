fd3d48f31d411a051c9ebf058a03ee5b5f745c91
Sebastian Ferrer <ferrers1023@gmail.com>
2025-09-07 01:22:13 -0400

    CHANGE code now iterates through page 1 properly, but does not grab pricing, or iterate to other pages

--- psn_webscraper.py
+++ psn_webscraper.py
@@ -13,7 +13,7 @@ class game:
     def __str__(self):
         return self.title + " " + self.sale_price + " " + self.regular_price
     
-my_url = "https://store.playstation.com/en-us/pages/deals"
+my_url = "https://store.playstation.com/en-us/category/3f772501-f6f8-49b7-abac-874a88ca4897/"
 
 def getPageHTML(url):
     "opening connection and grabbing the first page, returns a page_soup"
@@ -27,7 +27,7 @@ def sort_by_price(game):
     return str(game.sale_price)
 
 # get all the containers from the page listing the sales
-containers = getPageHTML(my_url).findAll("div", {"class": "psw-product-tile psw-interactive-root"})
+containers = getPageHTML(my_url).find_all("div", {"class": "psw-product-tile psw-interactive-root"})
 links = []
 
 #pull the URL links for each sale page
@@ -40,12 +40,12 @@ for container in containers:
 sale_pages = []
 
 for link in links:
-    sale_page = getPageHTML("https://store.playstation.com" + link["href"]) # + "/1?gameContentType=games%2Cbundles%2Caddons")
+    sale_page = getPageHTML(my_url)
     sale_pages.append(sale_page)
 
 #add each page after the first to the sale_page list
 for sale_page in sale_pages:
-    nextpage = sale_page.find("a", {"class" : "psw-icon psw-icon--chevron-right psw-icon psw-icon-size2 psw-icon--chevron-right"})
+    nextpage = sale_page.find("a", {"data-qa" : "ems-sdk-grid#ems-sdk-top-paginator-root#next"})
     if nextpage is not None:
         sale_pages.append(getPageHTML(my_url + nextpage["href"]))
 
@@ -55,7 +55,7 @@ games = []
 #iterate through each sale page that was grabbed
 for sale_page in sale_pages:
 
-    cells = sale_page.findAll("div", {"class" : "psw-product-tile psw-interactive-root"})
+    cells = sale_page.find_all("div", {"class" : "psw-product-tile psw-interactive-root"})
     #from every cell, make a game object with its name and prices and add it to a list
     for cell in cells:
 
@@ -64,10 +64,10 @@ for sale_page in sale_pages:
             reg_price = "0.00"
         else:
             reg_price = float(cell.find("span", {"data-qa" : "ems-sdk-grid#productTile0#price#display-price"}).getText().replace("$",""))
-        if cell.find("h3", {"data-qa" : "ems-sdk-grid#productTile0#price#display__price"}) is None:
+        if cell.find("h3", {"data-qa" : "ems-sdk-grid#productTile0#price#price-strikethrough"}) is None:
             sale_price = "0.00"
         else: 
-            sale_price = float(cell.find("h3", {"data-qa" : "ems-sdk-grid#productTile0#price#display__price"}).getText().replace("$",""))
+            sale_price = float(cell.find("h3", {"data-qa" : "ems-sdk-grid#productTile0#price#price-strikethrough"}).getText().replace("$",""))
 
         games.append(game(title, sale_price, reg_price))
 
@@ -76,8 +76,8 @@ games.sort(reverse=True,key=sort_by_price)
 
 #adding the list to a csv file named for today's date
 date = str(datetime.today().strftime("%b-%d-%Y"))
-textfilename = "D:\python\psnscraper\games" + date + ".txt"
-csvfilename = "D:\python\psnscraper\games" + date + ".csv"
+textfilename = "D:\\python\\psnscraper\\games" + date + ".txt"
+csvfilename = "D:\\python\\psnscraper\\games" + date + ".csv"
 
 f2 = open(csvfilename, "w")
 f2.write("Title,Sale Price,Regular Price\n")
@@ -88,44 +88,3 @@ for x in games:
         continue
 
 f2.close()
-"""
-date = str(datetime.today().strftime("%b%d%Y"))
-conn = db.connect('psn.db')
-conn.row_factory = db.Row
-cursor = conn.execute('select * from PSN_PRICE_TRACKING')
-row = cursor.fetchone()
-names = row.keys()
-
-for name in names:
-    if name is date:
-        exit
-    
-cursor = conn.execute("ALTER TABLE PSN_PRICE_TRACKING ADD COLUMN {};".format(date))
-
-game_list = cursor.fetchall(cursor.execute("select name from table;"))
-
-for game in games:
-    game_name = str(game.title.replace('\'',"").replace('\"',"").replace(",","|").replace("\t","").replace(";","|"))
-    cursor.execute("INSERT INTO PSN_PRICE_TRACKING VALUES({}, '{}', {}, {});".format(name , game.regular_price, game.sale_price))
-    print("record inserted")
-
-#cursor.execute("create TABLE PSN_PRICE_TRACKING (gameID INTEGER PRIMARY KEY, name TEXT, regular_price INTEGER, {0} smallint INTEGER)".format(date))
-#print("Table created")
-
-
-#i = 0
-
-#for game in games:
-#    i = i + 1
-#    name = str(game.title.replace('\'',"").replace('\"',"").replace(",","|").replace("\t","").replace(";","|"))
-#    cursor.execute("INSERT INTO PSN_PRICE_TRACKING VALUES({}, '{}', {}, {});".format(i, name , game.regular_price, game.sale_price))
-#    print("record inserted")
-
-
-    
-cursor.execute("SELECT * FROM PSN_PRICE_TRACKING")
-print(cursor.fetchall())
-conn.commit()   
-cursor.close
- 
-"""
